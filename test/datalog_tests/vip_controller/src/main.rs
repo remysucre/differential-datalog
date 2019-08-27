804678fc603279c1f592695069631932f139f621
Remy Wang <remy.sucre@gmail.com>
2019-08-27 14:32:21 -0700

    Add main drivers for vip_fwd tests

---
+++ test/datalog_tests/vip_controller/src/main.rs
@@ -0,0 +1,107 @@
+use differential_datalog::program::Update;
+use differential_datalog::record::{Record, UpdCmd, RelIdentifier};
+use observe::{Observer, Observable, Subscription};
+use tcp_channel::{TcpSender, ATcpSender, TcpReceiver};
+
+use vip_fwd_ddlog::*;
+use vip_fwd_ddlog::api::*;
+use vip_fwd_ddlog::Relations::*;
+
+use std::net::SocketAddr;
+use std::collections::{HashSet, HashMap};
+use std::sync::{Arc, Mutex};
+use std::borrow::Cow;
+
+fn main() {
+    // Construct server
+    let prog = HDDlog::run(1, false, |_,_:&Record, _| {});
+    let redirect: HashMap<_, _> =
+        vec![(vip_fwd_host_VM as usize,
+              vip_fwd_controller_VM as usize)]
+        .into_iter().collect();
+    let mut s = server::DDlogServer::new(prog, redirect);
+
+    // Receiving channel 1
+    let addr_s = "127.0.0.1:8001";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut receiver1 = TcpReceiver::new(addr);
+    let rec_con_1 = receiver1.connect();
+
+    // Receiving channel 2
+    let addr_s = "127.0.0.1:8002";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut receiver2 = TcpReceiver::new(addr);
+    let rec_con_2 = receiver2.connect();
+
+    // Sending TCP channel 1
+    let addr_s = "127.0.0.1:8010";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut sender1 = TcpSender::new(addr);
+    sender1.connect();
+
+    // Sending TCP channel 2
+    let addr_s = "127.0.0.1:8020";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut sender2 = TcpSender::new(addr);
+    sender2.connect();
+
+    // Wait for receiver connections
+    rec_con_1.join();
+    rec_con_2.join();
+
+    // Stream table from left server
+    let mut tup = HashSet::new();
+    tup.insert(vip_fwd_controller_FwdTable as usize);
+    let mut outlet = s.add_stream(tup);
+
+    // Server1 subscribes to the upstream TCP channel
+    let s_a = Arc::new(Mutex::new(s));
+    let _sub = {
+        let s_a = server::ADDlogServer(s_a.clone());
+        receiver1.subscribe(Box::new(s_a))
+    };
+
+    // Server2 subscribes to the upstream TCP channel
+    let _sub = {
+        let s_a = server::ADDlogServer(s_a.clone());
+        receiver2.subscribe(Box::new(s_a))
+    };
+
+    // Downstream TCP channel subscribes to the stream
+    let _sub = {
+        outlet.subscribe(Box::new(sender1))
+    };
+
+    let _sub = {
+        outlet.subscribe(Box::new(sender2))
+    };
+
+    // Insert `true` to Left in left server
+    let rec = Record::NamedStruct(
+        Cow::from("vip_fwd.controller.Host"),
+        vec![(Cow::from("id"), Record::Bool(true)),
+             (Cow::from("ip"), Record::Bool(true))]);
+    let table_id = RelIdentifier::RelId(vip_fwd_controller_Host as usize);
+    let updates = &[UpdCmd::Insert(table_id, rec)];
+
+    let handle1 = receiver1.listen();
+    let handle2 = receiver2.listen();
+
+    // Execute and transmit the update
+    {
+        let mut s = s_a.lock().unwrap();
+
+        s.on_start();
+        s.on_updates(Box::new(updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap())));
+        s.on_commit();
+    }
+
+    //sender.lock().unwrap().disconnect();
+    handle1.join();
+    handle2.join();
+
+    //receiver.disconnect();
+
+    let mut s = s_a.lock().unwrap();
+    s.shutdown();
+}
---
+++ test/datalog_tests/vip_host_b/src/main.rs
@@ -0,0 +1,87 @@
+use differential_datalog::program::Update;
+use differential_datalog::record::{Record, UpdCmd, RelIdentifier};
+use observe::{Observer, Observable, Subscription};
+use tcp_channel::{TcpSender, ATcpSender, TcpReceiver};
+
+use vip_fwd_ddlog::*;
+use vip_fwd_ddlog::api::*;
+use vip_fwd_ddlog::Relations::*;
+
+use std::net::SocketAddr;
+use std::collections::{HashSet, HashMap};
+use std::sync::{Arc, Mutex};
+use std::borrow::Cow;
+
+fn main() {
+    // Construct server
+    let prog = HDDlog::run(1, false, |_,_:&Record, _| {});
+    let redirect: HashMap<_, _> =
+        vec![(vip_fwd_controller_FwdTable as usize,
+              vip_fwd_host_FwdTable_ as usize)]
+        .into_iter().collect();
+    let mut s = server::DDlogServer::new(prog, redirect);
+
+    // Receiving channel
+    let addr_s = "127.0.0.1:8020";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut receiver = TcpReceiver::new(addr);
+    let rec_con = receiver.connect();
+
+    // Sending TCP channel
+    let addr_s = "127.0.0.1:8002";
+    let addr = addr_s.parse::<SocketAddr>().unwrap();
+    let mut sender = TcpSender::new(addr);
+    sender.connect();
+
+    // Wait for receiver connections
+    rec_con.join();
+
+    // Stream table from left server
+    let tup: HashSet<_> = vec![vip_fwd_host_VM as usize]
+        .into_iter().collect();
+    let mut outlet = s.add_stream(tup);
+
+    // Server subscribes to the upstream TCP channel
+    let s_a = Arc::new(Mutex::new(s));
+    let _sub = {
+        let s_a = server::ADDlogServer(s_a.clone());
+        receiver.subscribe(Box::new(s_a))
+    };
+
+    // Downstream TCP channel subscribes to the stream
+    let _sub = {
+        outlet.subscribe(Box::new(sender))
+    };
+
+    // Insert `true` to Left in left server
+    let host_val = Record::NamedStruct(
+        Cow::from("vip_fwd.host.HostId"),
+        vec![(Cow::from("id"), Record::Bool(true))]);
+    let host_id = RelIdentifier::RelId(vip_fwd_host_HostId as usize);
+
+    let vm_val = Record::NamedStruct(
+        Cow::from("vip_fwd.host.VM_"),
+        vec![(Cow::from("vip"), Record::Bool(true))]);
+    let vm_id = RelIdentifier::RelId(vip_fwd_host_VM_ as usize);
+
+    let updates = &[UpdCmd::Insert(host_id, host_val),
+                    UpdCmd::Insert(vm_id, vm_val)];
+
+    let handle = receiver.listen();
+
+    // Execute and transmit the update
+    {
+        let mut s = s_a.lock().unwrap();
+        s.on_start();
+        s.on_updates(Box::new(updates.into_iter().map(|cmd| updcmd2upd(cmd).unwrap())));
+        s.on_commit();
+    }
+
+    //sender.lock().unwrap().disconnect();
+    handle.join();
+
+    //receiver.disconnect();
+
+    let mut s = s_a.lock().unwrap();
+    s.shutdown();
+}
